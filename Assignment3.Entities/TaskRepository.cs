9531674e8d70c8571367045e42078d01b620ef53
JonasUJ <jonas@ulsnes.dk>
2022-09-28 21:51:42 +0200

    Convert Delete to use switch

--- Assignment3.Entities/TaskRepository.cs
+++ Assignment3.Entities/TaskRepository.cs
@@ -33,35 +33,26 @@ public class TaskRepository : ITaskRepository
     public Response Delete(int taskId)
     {
         var entity = context.Tasks.FirstOrDefault(c => c.Id == taskId);
-        //Need to do this here? But only in this method?
-        Response res = new Response();
 
-        if (entity is not null)
+        switch (entity?.State)
         {
-            if (entity.State.Equals(State.Active))
-            {
+            case State.Active:
                 entity.State = State.Removed;
                 context.SaveChanges();
-
-                //Should set response to what? Delete?
-                res = Response.Deleted;
-            }
-            else if (entity.State.Equals(State.Resolved) || entity.State.Equals(State.Closed) || entity.State.Equals(State.Removed))
-            {
-                res = Response.Conflict;
-            }
-            else if (entity.State.Equals(State.New))
-            {
+                return Response.Updated;
+            case State.Resolved:
+            case State.Closed:
+            case State.Removed:
+                return Response.Conflict;
+            case State.New:
                 context.Tasks.Remove(entity);
                 context.SaveChanges();
-                res = Response.Deleted;
-            }
+                return Response.Deleted;
+            case null:
+                return Response.NotFound;
+            default:
+                throw new NotImplementedException("State variant not implemented");
         }
-        else
-        {
-            res = Response.NotFound;
-        }
-        return res;
     }
 
     public TaskDetailsDTO Read(int taskId)
